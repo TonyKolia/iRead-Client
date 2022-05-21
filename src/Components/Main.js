299d6c7252a59cb5ced767318653b22d73ef5345
TonyKolia <tonykolia@gmail.com>
2022-05-21 14:00:27 +0300

    sidebar toggle functionality

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -15,18 +15,40 @@ export default function Main() {
         setYears({ minYear: 0, maxYear: 9999 });
     }
 
+    const toggleSidebar = () =>{
+        let sidebar = document.getElementById("sidebar");
+        let content = document.getElementById("booksContent");
+        if(sidebar.classList.contains("hide")){ //show
+
+            sidebar.classList.add("show");
+            sidebar.classList.remove("hide");
+
+            content.classList.add("retract-content");
+            content.classList.remove("expand-content");
+            setTimeout(() => content.style.marginLeft = "230px", 1000);
+        }
+        else{
+            sidebar.classList.add("hide");
+            sidebar.classList.remove("show");
+
+            content.classList.add("expand-content");
+            content.classList.remove("retract-content");
+            setTimeout(() => content.style.marginLeft = "0px", 1000);
+        }
+    }
+
     return (
         <div>
             <Sidebar setCategory={setCategory} setFilters={setFilters} setYears={setYears} category={category} filters={filters} years={years} />
             <div className="toolbar-container">
-                <button type="button" className="btn btn-primary btn-custom" data-bs-toggle="collapse" data-bs-target="#sidebar" role="button" aria-expanded="false" aria-controls="sidebar"><i className="fa-solid fa-arrow-left"></i></button>
+                <button type="button" onClick={toggleSidebar} className="btn btn-primary btn-custom"><i className="fa-solid fa-arrow-left"></i></button>
                 <button type="button" onClick={() => { reset(); }} className="btn btn-primary btn-custom"><i className="fa-solid fa-rotate"></i></button>
                 <div className="search-container">
                     <input type="text" className="search-control" placeholder="Αναζήτηση..." />
                     <button type="button" className="btn btn-primary btn-custom btn-search"><i className="fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </div>
-            <div className="content">
+            <div className="content" id="booksContent">
                 <BookItems category={category} filters={filters} years={years} />
             </div>
         </div>
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -17,25 +17,23 @@ export default function Sidebar(props) {
     }, []);
 
     return (
-        <div className="collapse show" id="sidebar">
-            <div className="sidebar">
-                <div className="accordion" id="accordionExample">
-                    <div className="accordion-item">
-                        <h5 className="accordion-header" id="headingOne">
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <h5><i className="fa-solid fa-filter"></i>Φίλτρα</h5>
-                            </button>
-                        </h5>
-                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" id="filterData">
-                                <Authors authors={criteria.authors} setFilters={props.setFilters} selectedAuthors={props.filters.authors} setCategory={props.setCategory} />
-                                <Publishers publishers={criteria.publishers} setFilters={props.setFilters} selectedPublishers={props.filters.publishers} setCategory={props.setCategory} />
-                                <Years years={{ minYear: criteria.minYear, maxYear: criteria.maxYear }} selectedYears = {props.years}  setYears={props.setYears} setCategory={props.setCategory} />
-                            </div>
+        <div className="sidebar" id="sidebar">
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h5 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <h5><i className="fa-solid fa-filter"></i>Φίλτρα</h5>
+                        </button>
+                    </h5>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" id="filterData">
+                            <Authors authors={criteria.authors} setFilters={props.setFilters} selectedAuthors={props.filters.authors} setCategory={props.setCategory} />
+                            <Publishers publishers={criteria.publishers} setFilters={props.setFilters} selectedPublishers={props.filters.publishers} setCategory={props.setCategory} />
+                            <Years years={{ minYear: criteria.minYear, maxYear: criteria.maxYear }} selectedYears={props.years} setYears={props.setYears} setCategory={props.setCategory} />
                         </div>
                     </div>
-                    <Categories categories={criteria.categories} setCategory={props.setCategory} setFilters={props.setFilters} selectedCategory={props.category} setYears={props.setYears} />
                 </div>
+                <Categories categories={criteria.categories} setCategory={props.setCategory} setFilters={props.setFilters} selectedCategory={props.category} setYears={props.setYears} />
             </div>
         </div>
     );
