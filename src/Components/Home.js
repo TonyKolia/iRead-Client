69531317a3e14c13d6f8219ad2f6c73df8c6f684
TonyKolia <tonykolia@gmail.com>
2022-05-22 17:43:29 +0300

    even more css  - home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,6 +11,8 @@ export default function Home() {
 
     return (
         <div>
+
+
             <div id="carouselExampleIndicators" className="carousel slide home-page-carousel" data-ride="carousel">
                 <ol className="carousel-indicators">
                     <li key="img1" data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
@@ -18,8 +20,27 @@ export default function Home() {
                     <li key="img3" data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                 </ol>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img className="d-block w-100" src={carouselTemp} alt="First slide" />
+                    <div className="carousel-item carousel-item-container active">
+                        <div className="carousel-info">
+                            <span>
+                            <h3>Η αγαπημένη σας βιβλιοθήκη, τώρα και online!</h3>
+                            <br/>
+                            <p>
+                                Ανακαλύψτε τα αγαπημένα σας βιβλία, πραγματοποιήστε online την κράτηση σας και παραλάβετε άμεσα απ' την βιβλιοθήκη.
+                            </p>
+                            <p>
+                                Με πάνω από 1000 διαφορετικούς τίτλους είναι σίγουρο ότι θα βρείτε κάτι για εσάς.
+                            </p>
+                            <p>
+                                Τόσο ευχάριστα, τόσο γρήγορα, τόσο απλά!
+                            </p>
+
+                            </span>
+                        </div>    
+                        <div className="carousel-img-container" >
+                        <img className="carousel-img"  src={require("../Images/carousel-img-1.jpg")} alt="First slide" />
+                        </div>
+                        
                     </div>
                     <div className="carousel-item">
                         <img className="d-block w-100" src={carouselTemp} alt="Second slide" />
@@ -29,6 +50,9 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+
+
+
 
             <div style={{ marginTop: "10px" }}>
                 <ul className="nav justify-content-center mini-menu">
